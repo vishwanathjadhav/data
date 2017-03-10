be23df64939000c5ce8b5150eb3bf9b963f24383
vishwa <vjadhav@redhat.com>
2017-03-10 11:51:19 +0530

    Data: 1.0.0.0 dns file found test

---
+++ dns_reslover.py
@@ -0,0 +1,12 @@
+#/usr/bin/env python
+"""
+this is the test rule to check if there is resolve.conf is already present.
+If not present create one and add gateway for the same.
+
+"""
+
+def print_all():
+    print "Hello World\n"
+
+
+print_all()
